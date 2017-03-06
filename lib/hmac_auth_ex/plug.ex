b3762e6b3a29010893ad1af1d2cfaaa00235453e
Zach Kemp <zvkemp@gmail.com>
2017-03-06 11:37:12 -0800

    plug adapter

---
+++ lib/hmac_auth_ex/plug.ex
@@ -0,0 +1,39 @@
+defmodule HMACAuthEx.Plug do
+  import Plug.Conn
+
+  def init(options), do: options
+
+  def call(conn, _opts) do
+    Plug.Conn.put_private(conn, :hmac_verified, case verify_hmac(conn) do
+      :ok -> true
+      _   -> false
+    end)
+  end
+
+  defp verify_hmac(conn) do
+    with %{
+      "x-signature" => signature,
+      "x-request-id" => request_id
+    } <- Enum.into(conn.req_headers, %{}),
+    {:ok, body, _} <- Plug.Conn.read_body(conn)
+    do
+      [key|_] = String.split(request_id, "-")
+      HMACAuth.verify(%{
+        path: conn.request_path,
+        secret: hmac_secret(key),
+        method: conn.method,
+        request_id: request_id,
+        signature: signature,
+        data: body,
+        timestamp: nil,
+        ttl: Application.get_env(:hmac_auth_ex, :ttl, 3)
+      }) && :ok || :error
+    else
+      _ -> :error
+    end
+  end
+
+  defp hmac_secret(key) do
+    with %{^key => secret} <- Application.get_env(:hmac_auth_ex, :keys), do: secret
+  end
+end
